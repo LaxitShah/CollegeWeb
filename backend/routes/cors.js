d9d91b0b5528af90801169d3aa6a9974db7cea5c
Arkan Mansuri <arkanmansuri88@gmail.com>
2023-08-03 23:42:13 +0530

    Update1

--- backend/routes/cors.js
+++ backend/routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3444', 'https://localhost:4200', 'https://collegeweb-backend-6tic.onrender.com'];
+const whitelist = ['https://collegeweb-backend-6tic.onrender.com','http://localhost:3444', 'https://localhost:4200', ];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if(whitelist.indexOf(req.header('origin') !== -1)){
